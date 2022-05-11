cbdf6634ffc95989bc138cb562979ecd95234886
Hart Rockwell <hart.rockwell@gmail.com>
2022-05-10 21:39:34 -0500

    replace Justins code

--- code/grader/tests.py
+++ code/grader/tests.py
@@ -34,7 +34,7 @@ class FinalGrader(Grader):
     """Match against Instructor/TA's agents"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.student_model = HockyRunner(self.module.Team())
+        self.student_model = HockyRunner(self.module.Team(team_kart_list=TEAM_KART_LIST, agent_target_speed_list=AGENT_TARGET_SPEED, time_act_func=PRINT_TEAM_ACT_EXECUTION))
         self.match = Match(use_graphics=self.student_model.agent_type == 'image')
 
     def _test(self, agent_name):
@@ -56,16 +56,12 @@ class FinalGrader(Grader):
         results = []
         try:
             for bl in ball_locations:
-                result = self.match.run(self.student_model, test_model, 2, STEPS_PER_MATCH, max_score=3,
-                                   initial_ball_location=bl, initial_ball_velocity=[0, 0],
-                                   record_fn=None, timeout=time_limit, verbose=self.verbose)
+                result = self.match.run(self.student_model, test_model, 2, STEPS_PER_MATCH, max_score=3, initial_ball_location=bl, initial_ball_velocity=[0, 0], record_fn=None, timeout=time_limit, verbose=self.verbose)
                 scores.append(result[0])
                 results.append(f'{result[0]}:{result[1]}')
 
             for bl in ball_locations:
-                result = self.match.run(test_model, self.student_model, 2, STEPS_PER_MATCH, max_score=3,
-                                   initial_ball_location=bl, initial_ball_velocity=[0, 0],
-                                   record_fn=None, timeout=time_limit, verbose=self.verbose)
+                result = self.match.run(test_model, self.student_model, 2, STEPS_PER_MATCH, max_score=3, initial_ball_location=bl, initial_ball_velocity=[0, 0], record_fn=None, timeout=time_limit, verbose=self.verbose)
                 scores.append(result[1])
                 results.append(f'{result[1]}:{result[0]}')
         except MatchException as e:
@@ -73,31 +69,32 @@ class FinalGrader(Grader):
             print(' T1:', e.msg1)
             print(' T2:', e.msg2)
             assert 0
-        return sum(scores), results
+        return sum(scores), results, ball_locations
 
     @Case(score=25)
     def test_geoffrey(self):
         """geoffrey agent"""
-        scores, results = self._test('geoffrey_agent')
-        return min(scores / len(results), 1), "{} goals scored in {} games ({})".format(scores, len(results), '  '.join(results))
+        scores, results, ball_locations = self._test('geoffrey_agent')
+        return min(scores / len(results), 1), f"{scores} goals scored in {len(results)} games ({'  '.join(results)}) using starting locations {ball_locations}"
 
-    @Case(score=25)
+@Case(score=25)
     def test_yann(self):
         """yann agent"""
-        scores, results = self._test('yann_agent')
-        return min(scores / len(results), 1), "{} goals scored in {} games ({})".format(scores, len(results), '  '.join(results))
+        scores, results, ball_locations = self._test('yann_agent')
+        return min(scores / len(results), 1), f"{scores} goals scored in {len(results)} games ({'  '.join(results)}) using starting locations {ball_locations}"
 
-    @Case(score=25)
+@Case(score=25)
     def test_yoshua(self):
         """yoshua agent"""
-        scores, results = self._test('yoshua_agent')
-        return min(scores / len(results), 1), "{} goals scored in {} games ({})".format(scores, len(results), '  '.join(results))
+        scores, results, ball_locations = self._test('yoshua_agent')
+        return min(scores / len(results), 1), f"{scores} goals scored in {len(results)} games ({'  '.join(results)}) using starting locations {ball_locations}"
 
-    @Case(score=25)
+
+@Case(score=25)
     def test_jurgen(self):
         """jurgen agent"""
         if self.student_model.agent_type == 'state':
-            scores, results = self._test('jurgen_agent')
+            scores, results, ball_locations = self._test('jurgen_agent')
         else:
-            scores, results = self._test('image_jurgen_agent')
-        return min(scores / len(results), 1), "{} goals scored in {} games ({})".format(scores, len(results), '  '.join(results))
+            scores, results, ball_locations = self._test('image_jurgen_agent')
+            return min(scores / len(results), 1), f"{scores} goals scored in {len(results)} games ({'  '.join(results)}) using starting locations {ball_locations}"
